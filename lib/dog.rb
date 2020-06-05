6fe679ebb9bbb592501ed57a5ffde004cc05c488
Abelardo Miranda <available_email_address@yahoo.com>
2020-06-05 14:32:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Add your code here
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+    def Dog.all
+      @@all
+    end
+
+    def Dog.print_all
+      puts @@all
+    end
+
+end
